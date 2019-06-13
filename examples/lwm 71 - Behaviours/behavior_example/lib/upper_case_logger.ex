c23bb135ac3fd21e532547caf903d0399b414fb9
Maultasche <kevin.peter@gmail.com>
2019-06-13 11:45:49 -0700

    Updated lwm 71 examples

--- examples/lwm 71 - Behaviours/behavior_example/lib/upper_case_logger.ex
+++ examples/lwm 71 - Behaviours/behavior_example/lib/upper_case_logger.ex
@@ -3,11 +3,13 @@ defmodule BehaviorExample.UpperCaseLogger do
 
   alias BehaviorExample.Logger
 
+  @impl Logger
   @spec log_message(Logger.log_list(), Logger.level(), String.t()) :: Logger.log_list()
   def log_message(log_list, level, message) do
     [{level, String.upcase(message)} | log_list]
   end
 
+  @impl Logger
   @spec log_data(Logger.log_list(), Logger.level(), any()) :: Logger.log_list()
   def log_data(log_list, level, data) do
     [{level, inspect(data)} | log_list]
