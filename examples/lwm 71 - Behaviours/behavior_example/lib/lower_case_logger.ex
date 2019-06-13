25f9f0b7d8bfacf7011b6e80dc32eff231539638
Maultasche <kevin.peter@gmail.com>
2019-06-13 11:34:10 -0700

    Updated lwm 71 examples

--- examples/lwm 71 - Behaviours/behavior_example/lib/lower_case_logger.ex
+++ examples/lwm 71 - Behaviours/behavior_example/lib/lower_case_logger.ex
@@ -3,11 +3,13 @@ defmodule BehaviorExample.LowerCaseLogger do
 
   alias BehaviorExample.Logger
 
+  @impl Logger
   @spec log_message(Logger.log_list(), Logger.level(), String.t()) :: Logger.log_list()
   def log_message(log_list, level, message) do
     [{level, String.downcase(message)} | log_list]
   end
 
+  @impl Logger
   @spec log_data(Logger.log_list(), Logger.level(), any()) :: Logger.log_list()
   def log_data(log_list, level, data) do
     [{level, inspect(data)} | log_list]
